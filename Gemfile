4215651d8db20d2d68a3a1f63d9bd911d9217e93
catsinspacesuits <simunro@hotmail.co.uk>
2020-11-13 14:29:09 +0100

    Add functionality to get pokemon and species from API

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'sinatra'
 gem 'haml'
 
 group :development do
+  gem 'poke-api-v2'
   gem 'shotgun'
   gem 'rake'
   gem 'rack-test'
--- pokedex_app.rb
+++ pokedex_app.rb
@@ -1,8 +1,18 @@
 class PokedexApp < Sinatra::Application
   enable :sessions
+  require 'poke-api-v2'
 
   get '/' do
     display_page :index
+  end
+
+  def get_pokemon
+    @random_pokemon_id = rand(1..248)
+    @pokemon = PokeApi.get(pokemon: @random_pokemon_id)
+  end
+
+  def get_pokemon_species
+    @pokemon_species = PokeApi.get(pokemon_species: @random_pokemon_id)
   end
 
   def display_page(location, locals = {})
@@ -11,11 +21,13 @@ class PokedexApp < Sinatra::Application
       layout: locals.fetch(:layout) { :default },
       locals: locals
     }
+    get_pokemon
+    get_pokemon_species
 
-    haml location.to_sym, options
+    erb location.to_sym, options
   end
 
   def display_partial(location, locals = {})
-    haml location.to_sym, layout: false, locals: locals
+    erb location.to_sym, layout: false, locals: locals
   end
 end
